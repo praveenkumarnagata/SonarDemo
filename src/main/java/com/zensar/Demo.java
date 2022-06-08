86d8f478de679496301f50dda141683a8eb35067
praveenkumarnagata <104186726+praveenkumarnagata@users.noreply.github.com>
2022-06-08 15:18:16 +0530

    Add files via upload

---
+++ src/main/java/com/zensar/Demo.java
@@ -0,0 +1,22 @@
+package com.zensar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo {
+	public Demo() {
+		System.out.println("In constructor");
+	}
+	 public void calculateData()
+	    {
+	        String s = "";
+	        List<Integer> list = new ArrayList<Integer>();
+	        list.add(1);
+	        list.add(2);
+
+
+	        Object obj = null;
+	        System.out.println(obj.toString());
+	    }
+
+}
